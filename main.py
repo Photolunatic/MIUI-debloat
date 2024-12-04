0cb0f147ade79656ce0959b063f1c3ecb29770a3
Seb <76048167+Photolunatic@users.noreply.github.com>
2024-12-04 10:33:06 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from debloat_checker import *
 
 file_path = 'app.txt'
 
-with open(file_path, 'r', encoding='utf-16') as f:
+# Open the file with the correct encoding (UTF-8, which will work for US-ASCII as well)
+with open(file_path, 'r', encoding='utf-8') as f:
     lines = f.readlines()
 
 items = [line.replace('package:', '', 1).strip() for line in lines]
@@ -11,17 +12,22 @@ items = [line.replace('package:', '', 1).strip() for line in lines]
 uninstall_list, remaining_list = checkDebloat(items)
 uninstall_list.sort()
 
-# Create directory if the directory not exists
+# Create directory if the directory does not exist
 if not os.path.exists("output"):
-     os.makedirs("output")
+    os.makedirs("output")
+
 # Open the output file to write
 with open("output/uninstall_list.txt", 'w') as f_out:
     # Write each item to the output file
     for item in uninstall_list:
-        f_out.write('platform-tools/adb.exe shell pm uninstall --user 0 ' + item + '\n')
+        # Reference the adb in the current folder
+        f_out.write('./adb shell pm uninstall --user 0 ' + item + '\n')
+
 remaining_list.sort()
+
 # Open the output file to write
 with open("output/remaining_list.txt", 'w') as f_out:
     # Write each item to the output file
     for item in remaining_list:
         f_out.write(item + '\n')
+
